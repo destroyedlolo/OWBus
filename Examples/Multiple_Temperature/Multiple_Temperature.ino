c82b9ac64c26d27f9f0c96fbbc06e60b30f76fde
destroyedlolo <l_faillie@yahoo.com>
2017-11-04 02:22:22 +0100

    WiP

---
+++ Examples/Multiple_Temperature/Multiple_Temperature.ino
@@ -0,0 +1,40 @@
+/* This example shows how to 
+ *	- enumerate temperature probes
+ *	- launch parallel temperature conversion
+ *
+ *  This little piece of code is put in Public Domain.
+ */
+
+#include <OWBus.h>
+#include <OWBus/DS18B20.h>
+#include <OWBus/DS28EA00.h>
+
+#define ONE_WIRE_BUS D1			// Where O1 bus is connected to
+OneWire oneWire(ONE_WIRE_BUS);	// Initialize oneWire library
+OWBus bus(&oneWire);
+
+void setup() {
+	Serial.begin(115200);
+	delay(100);
+}
+
+void loop() {
+	Serial.print("\nNumber of probes on the bus :");
+	Serial.println(bus.getDeviceCount());
+
+	Serial.println("Individual address :");
+	OWBus::Address addr;
+	bus.search_reset();
+	while( bus.search_next( addr ) ){
+		Serial.print( addr.toString().c_str() );
+		Serial.print(" : ");
+		if(!addr.isValide( &oneWire))
+			Serial.println("Invalid address");
+		else {
+			OWDevice probe( bus, addr );
+			Serial.println( probe.getFamilly() );
+		}
+	}
+
+	delay(30e3);	// Sleep for 30 seconds
+}
