e84cc75fe56d0b70139f4f1767dad35b128aaf86
destroyedlolo <l_faillie@yahoo.com>
2017-11-15 01:39:41 +0100

    Add ChannelInfo and StatusMemory

--- OWBus/DS2406.h
+++ OWBus/DS2406.h
@@ -8,52 +8,21 @@
 #include <OWBus.h>
 #include <OWBus/OWDevice.h>
 
-/* As per https://www.maximintegrated.com/en/app-notes/index.mvp/id/5856
- *
- * Channel control byte 1 : (2 is always 0xff)
- * -------------------------------------------
- * ALR = 1 clear both latches, 0 keep them unchanged
- * TOG = 0 (else toggling between read and write)
- * IM = 0 writing, 1 reading
- * IC = 0 if only single channel / 0 = async / 1 = sync (both updated simultaneously)
- * CHS0 = PIO.A
- * CHS1 = PIO.B
- * CRC1/0 = control CRC generation (0 = none)
- *
- * Channel info byte :
- * -------------------
- * 1/0: PIO.B/PIO.A flip-flop (0 = conducting)
- * 3/2: PIO.B/PIO.A sensed reading
- * 5/4: PIO.B/PIO.A latches
- * 6: 0 = A only, 1 = B present
- * 7:  0 = parasite, 1 = power 
- *
- * ==========================
- * STATUS Memory (addr 7)
- * -------------
- * 0: 
- * 	- WP? : Write protect a page
- * 	- BM? : 0 = used page
- * 1-4: Page redirection
- * 5: set to 0x00
- * 6: 0x00 on a 2406, if 0x00 on a 2407 = hidden mode
- * 7:
- * 	CSS0/4 : Condition search to be used with 0xEC
- * 		CSS4 = B
- * 		CSS3 = A (if all CSS4/3/0 set to 0, always reply to a search)
- * 		CSS2/1 = source
- * 			0b01 = latch (stat change detect - reset when ALR set to 1)
- * 			0b10 = transitor status (0 = on)
- * 			0b11 = PIO (actual value)
- * 		CSS0 = value
- * 	Flip-flop A/B : 0 conducting, 1 float (*)
- * 	Supply : 0 = parasite, 1 = power (*)
- *
- * (*) accessible through the Channel Access function
- */
+/* As per https://www.maximintegrated.com/en/app-notes/index.mvp/id/5856 */
 
 class DS2406 : public OWDevice {
 private:
+
+	/* Channel control byte 1 : (2 is always 0xff)
+	 * -------------------------------------------
+	 * ALR = 1 clear both latches, 0 keep them unchanged
+	 * TOG = 0 (else toggling between read and write)
+	 * IM = 0 writing, 1 reading
+	 * IC = 0 if only single channel / 0 = async / 1 = sync (both updated simultaneously)
+	 * CHS0 = PIO.A
+	 * CHS1 = PIO.B
+	 * CRC1/0 = control CRC generation (0 = none)
+	 */
 	union {
 		struct {
 			unsigned int crc : 2;
@@ -65,9 +34,70 @@ private:
 			unsigned int alr : 1;
 		} bits;
 		uint8_t byte;
-	} ChannelControl ;
+	} ChannelControl;
+
+	/* Channel info byte :
+	 * -------------------
+	 * 1/0: PIO.B/PIO.A flip-flop (0 = conducting)
+	 * 3/2: PIO.B/PIO.A sensed reading
+	 * 5/4: PIO.B/PIO.A latches
+	 * 6: 0 = A only, 1 = B present
+	 * 7:  0 = parasite, 1 = power 
+	 */
+	union {
+		struct {
+			unsigned int flipflopA : 1;
+			unsigned int flipflopB : 1;
+			unsigned int sensedA : 1;
+			unsigned int sensedB : 1;
+			unsigned int latchA : 1;
+			unsigned int latchB : 1;
+			unsigned int channels : 1;
+			unsigned int supply : 1;
+		} bits;
+		uint8_t byte;
+	} ChannelInfo;
+
+	/* STATUS Memory
+	 * -------------
+	 * 0: 
+	 * 	- WP? : Write protect a page
+	 * 	- BM? : 0 = used page
+	 * 1-4: Page redirection
+	 * 5: set to 0x00
+	 * 6: 0x00 on a 2406, if 0x00 on a 2407 = hidden mode
+	 * 7:
+	 * 	CSS0/4 : Condition search to be used with 0xEC
+	 * 		CSS4 = B
+	 * 		CSS3 = A (if all CSS4/3/0 set to 0, always reply to a search)
+	 * 		CSS2/1 = source
+	 * 			0b01 = latch (stat change detect - reset when ALR set to 1)
+	 * 			0b10 = transitor status (0 = on)
+	 * 			0b11 = PIO (actual value)
+	 * 		CSS0 = polarity
+	 * 	Flip-flop A/B : 0 conducting, 1 float (*)
+	 * 	Supply : 0 = parasite, 1 = power (*)
+	 *
+	 * (*) accessible through the Channel Access function
+	 */
+	union {
+		struct {
+			unsigned int polarity : 1;
+			unsigned int sources : 2;
+			unsigned int channelA : 1;
+			unsigned int channelB : 1;
+			unsigned int flipflopA : 1;
+			unsigned int flipflopB : 1;
+			unsigned int supply : 1;
+		} bits;
+		uint8_t byte;
+	} StatusMemory;
 
 public:
+	enum Source {	// Condition search sources
+		LATCH = 1, FLIPFLOP, PIO 
+	};
+
 	DS2406( OWBus &abus, OWBus::Address &aa, const char *aname=NULL ) : OWDevice( abus, aa, aname ) {}
 	DS2406( OWBus &abus, uint64_t aa, const char *aname=NULL ) : OWDevice( abus, aa, aname ) {}
 
